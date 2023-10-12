ec6e1c6a9371392b0629f7ac3e1ae3db4a515b62
harry-krish303 <110534083+harry-krish303@users.noreply.github.com>
2023-10-12 22:54:11 +0530

    Add files via upload

---
+++ Day 16/HotelManage/src/main/java/com/example/demo/Service/ListService.java
@@ -0,0 +1,27 @@
+package com.example.demo.Service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+import com.example.demo.Entity.ReservedList;
+import com.example.demo.Repository.ListRepository;
+
+
+@Service
+public class ListService {
+	
+	@Autowired
+	private ListRepository listRepo;
+	
+	public List<ReservedList> getData()
+	{
+		return listRepo.findAll();
+	}
+	
+	public void postData(ReservedList list) {
+		listRepo.save(list);
+	}
+}
